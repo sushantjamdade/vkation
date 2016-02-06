c531832ea4c2728f0d904bf546e5ba73c31b335a
sushantjamdade <sushant.jamdade@dotweb.in>
2016-02-06 19:43:20 +0530

    After Bootstrap Updation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,26 +36,78 @@ vkationapp1.controller('vkationcontroller',['$scope', '$http', function ($scope,
 }]);
 */
 
+
 var vkationapp1 = angular.module('vkationapp',[])
 vkationapp1.controller('vkationcontroller',['$scope', '$http', function ($scope, $http) 
 {
-	$scope.headertitle = true;
-	$scope.headerimg = true;
-	$scope.resort = false;
+	//Dropdwon for Place
+	$scope.placeArray = ["Karnataka","Bengaluru","Mysore","Mangalore","Bellary","Shimoga","Raichur","Udupi","Bhadravati","Mandya"];
+	
+	//Dropdwon for Guests
+	$scope.guestsArray = ["1 Guest","2 Guest","3 Guest","4 Guest","5 Guest","6 Guest","7 Guest","8 Guest","9 Guest","10 Guest"];
+	
+	$scope.headertitle=true;
+	$scope.headerimg=true;
+	$scope.resort=false;
 	$scope.swcyvp = false;
+	$scope.swcyvp_one = false;
+	$scope.swcyvp_two = false;
+	$scope.swcyvp_three = false;
+	$scope.swcyvp_four = false;
 	$scope.accommodation = function() 
+	{
+		$scope.headertitle=false;
+		$scope.headerimg=false;
+		$scope.resort=true;
+	}
+	
+	$scope.cfcyvp = function() 
+	{			
+		$scope.headertitle = false;
+		$scope.headerimg = false;
+		$scope.resort = true;
+		$scope.swcyvp = true;
+		$scope.swcyvp_one = true;
+		$scope.swcyvp_two = false;
+		$scope.swcyvp_three = false;
+		$scope.swcyvp_four = false;
+	}
+			
+	$scope.cfcyvp_two = function() 
 	{
 		$scope.headertitle = false;
 		$scope.headerimg = false;
 		$scope.resort = true;
-		$scope.swcyvp = false;
+		$scope.swcyvp = true;
+		$scope.swcyvp_one = false;
+		$scope.swcyvp_two = true;
+		$scope.swcyvp_three = false;
+		$scope.swcyvp_four = false;
 	}
 	
-	$scope.cfcyvp = function() 
+	$scope.cfcyvp_three = function() 
 	{
 		$scope.headertitle = false;
 		$scope.headerimg = false;
-		$scope.resort = false;
+		$scope.resort = true;
 		$scope.swcyvp = true;
+		$scope.swcyvp_one = false;
+		$scope.swcyvp_two = false;
+		$scope.swcyvp_three = true;
+		$scope.swcyvp_four = false;
 	}
+	
+	$scope.cfcyvp_four = function() 
+	{
+		$scope.headertitle = false;
+		$scope.headerimg = false;
+		$scope.resort = true;
+		$scope.swcyvp = true;
+		$scope.swcyvp_one = false;
+		$scope.swcyvp_two = false;
+		$scope.swcyvp_three = false;
+		$scope.swcyvp_four = true;
+	}
+	
+	
 }]);
